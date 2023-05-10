bc90e94758c5bae29ccdcd6f027f7be99a33ff77
a-tibi <ahmd_tibi@yahoo.com>
2023-05-10 19:46:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Print_Generic
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Print<int>(10);
+			Print<string>("Ahmad");
+			Print<char>('T');
+			Print(new { Id = 1, Name ="Ahmad Tibi" });
+			Print<Person>(new Person("Ahmad","Tibi"));	
+			Console.ReadKey();
+		}
+
+		static void Print<T>(T value)
+		{
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine($"Data Type: {typeof(T)}");
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine($"Data Value: {value}");
+			Console.WriteLine("--------------------------------------------");
+
+		}
+	}
+
+	class Person
+	{
+		private string _fName;
+		private string _lLame;
+
+		public Person(string fName, string lName)
+		{
+			_fName = fName;
+			_lLame = lName;
+		}
+
+		public override string ToString()
+		{
+			return $"Person Name: {_fName} {_lLame}";
+		}
+	}
+}
